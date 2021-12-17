fe0d405f0f62593e7eae3cc3adca1c83ddb25baf
Kazi Islam <kazichaska@gmail.com>
2021-12-16 18:44:11 -0600

    created an array with all cases in it

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var specialChar = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/","\:","\;","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
 var numericChar = ["1","2","3","4","5","6","7","8","9","0"];
 var password = "";
 
@@ -60,16 +60,15 @@ function generatePassword() {
 
   if (!pass.upperOn && !pass.specialOn && !pass.lowerOn && !pass.numberOn) {
     window.alert("Has to be a valid option!");
-    console.log(result);
   }
   else if (pass.upperOn && pass.specialOn && pass.lowerOn && pass.numberOn) {
-    result = character.concat()
+    result = (upperCase + lowerCase + specialChar + numericChar);
+    console.log(result);
     }
-  }
-  else
+    else
     window.alert("Wrong value");
+  }
 
-}
 
 generatePassword();
 
